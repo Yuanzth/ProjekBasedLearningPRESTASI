8d708c716572f0cdbc9e941fa85977bd18d9ca9f
AlKhrisW <aldokhrisna@gmail.com>
2024-11-25 11:30:55 +0700

    repair

---
+++ aldo/konek.php
@@ -0,0 +1,5 @@
+<?php
+    $serverName = "LAPTOP-BCLGTLTG"; // serverName\instanceName
+    $connectionInfo = array("Database" => "db_prestasi_nonakademik"); // Ganti nama database di sini
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
